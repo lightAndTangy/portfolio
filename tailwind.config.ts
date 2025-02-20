0653de9688b3c18519007c8a6c689d680b98362f
lightAndTangy <a.xinyuetang@gmail.com>
2025-02-20 22:11:42 +1100

    added fonts and basic colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,9 +9,17 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        "bg": "#F6F3EE"
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "blue": "#7785C2",
+        "orange": "#E6BA83",
+        "text-normal": "#3A3A3A"
+      },
+      fontFamily: {
+        anek: ["Anek", "sans-serif"],
+        aileron: ["Aileron", "sans-serif"],
       },
     },
   },
