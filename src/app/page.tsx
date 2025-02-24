590025345cbfabc8e9c5ace429aa53150d5c8e39
lightAndTangy <a.xinyuetang@gmail.com>
2025-02-24 16:38:37 +1100

    updated imports in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import Homepage from "./components/home";
-import Navbar from "./components/navbar";
 export default function Home() {
   return (
     <div className="grid place-items-center min-h-screen p-8 pb-20 gap-4 sm:p-20 bg-bg text-center">
