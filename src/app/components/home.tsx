a38bf085c7c2382a6e3402f2b8b15a1e0c48bd18
lightAndTangy <a.xinyuetang@gmail.com>
2025-02-24 16:36:29 +1100

    fix linting

--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return(
     <div className="flex flex-col items-start">
         <h1 className="text-text-normal font-anek font-bold text-[80px]">
-          Hi, I'm Alice!
+          Hi, I&apos;m Alice!
         </h1>
         <p className="font-aileron font-semibold text-xl">
           Computer Science Graduate
