6acbea9c7b0b45a4c002725bce37e6ffcc5a1bfe
lightAndTangy <a.xinyuetang@gmail.com>
2025-02-24 15:52:38 +1100

    updated favicon and home section

---
+++ src/app/components/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home() {
+  return(
+    <div className="flex flex-col items-start">
+        <h1 className="text-text-normal font-anek font-bold text-[80px]">
+          Hi, I'm Alice!
+        </h1>
+        <p className="font-aileron font-semibold text-xl">
+          Computer Science Graduate
+        </p>
+      </div>
+  );
+}
+
